bbff06a077f7415c88c7ef8923cfa03b53d37eed
caniuse <luqiangbovip@163.com>
2020-08-20 01:38:30 +0800

    1

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import { Button } from 'antd';
 const PageIndex = () => {
   return (
     <>
-      <div className="main">hello next2020</div>
+      <div className="main">hello next 2020-08-20 01:38</div>
       <Button>
         <Link href="/a">
           <a>go to a</a>
