bd64ce8f1fd6cda85478653ef07b50ba247278b1
caniuse <luqiangbovip@163.com>
2020-10-17 00:52:14 +0800

    🐛 修复bug

--- pages/api/issue/more.ts
+++ pages/api/issue/more.ts
@@ -1,10 +1,10 @@
-import { getArticleMore } from 'fetch/index';
+import { getArticleList } from 'fetch/index';
 
 export default async (req, res) => {
   const {
     query: { page },
   } = req;
-  const [err1, res1] = await getArticleMore(page * 1);
+  const [err1, res1] = await getArticleList({ page: page * 1 });
   if (err1) {
     res.status(500).json(null);
     return;
